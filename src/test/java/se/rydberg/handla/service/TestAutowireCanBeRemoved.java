a33700dafece20132909bd9a6409d86063b92f69
Erik Rydberg <erik.rydberg@intraservice.goteborg.se>
2018-01-23 22:45:41 +0100

    Något uppdaterad med nyare versioner av ramverk

--- src/test/java/se/rydberg/handla/service/TestAutowireCanBeRemoved.java
+++ src/test/java/se/rydberg/handla/service/TestAutowireCanBeRemoved.java
@@ -1,11 +1,10 @@
-package se.rydberg.handla;
+package se.rydberg.handla.service;
 
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,7 +16,6 @@ import se.rydberg.handla.service.ShopListService;
 public class TestAutowireCanBeRemoved {
     
     @Autowired
-    @Qualifier("shopListService")
     ShopListService shopListService;
     
     @Test
